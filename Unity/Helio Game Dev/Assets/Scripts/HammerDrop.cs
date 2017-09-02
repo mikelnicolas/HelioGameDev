061fd437536b3eaa320d3751824137342cc246b5
Anthony Romrell <anthony@artisandream.com>
2017-09-02 13:05:14 -0600

    Finished Hammer Sounds with Colliders and Audio

---
+++ Unity/Helio Game Dev/Assets/Scripts/HammerDrop.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerDrop : MonoBehaviour {
+
+	public AudioSource hammerSound;
+	void OnCollisionEnter () {
+		hammerSound.Play();
+	}
+
+}

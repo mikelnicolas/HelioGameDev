c54afbfeb1f1a44741eda8ba517c5bfef5ae35b6
Anthony Romrell <anthony@artisandream.com>
2017-09-05 20:35:47 -0600

    Classes Lession — Animals

---
+++ Unity/Helio Game Dev/Assets/Classes/Bird.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : Animal {
+
+	//Functions
+	//Fly
+
+	//Perameters
+	//Feather Count
+	//Feather Colors
+
+}
---
+++ Unity/Helio Game Dev/Assets/Classes/Mammal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mammal : Animal {
+
+	//Functions
+	//Live Birth
+	//Nurse
+
+	//Perameters
+	//Hair/Fur
+	//Hair Color
+	//BiPed/QuadraPed/MonoPed
+
+}

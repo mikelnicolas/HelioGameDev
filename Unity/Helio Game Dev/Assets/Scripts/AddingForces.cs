2c4b4ab338b9f7f2363f99d81c466de34349c219
Anthony Romrell <anthony@artisandream.com>
2017-09-09 11:53:25 -0600

    Inputs for force

--- Unity/Helio Game Dev/Assets/Scripts/AddingForces.cs
+++ Unity/Helio Game Dev/Assets/Scripts/AddingForces.cs
@@ -1,26 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class AddingForces : MonoBehaviour {
 
 	Rigidbody shipRigidbody;
 	public float force = 10;
-	public Vector3 forceVector;
 	void Start () {
 		shipRigidbody = GetComponent<Rigidbody>();
-	}
-	void Update () {
-		AddShipForce(KeyCode.UpArrow, Vector3.up*force);
-		AddShipForce(KeyCode.DownArrow, Vector3.down*force);
-		AddShipForce(KeyCode.LeftArrow, Vector3.forward*force);
-		AddShipForce(KeyCode.RightArrow, Vector3.back*force);
+		InputsStandard.StandardInput += AddShipForce;
+		InputsInverted.StandardInput += AddShipForce;
 	}
 
 	void AddShipForce (KeyCode _i, Vector3 _v) {
 		if (Input.GetKeyDown(_i))
 		{
-			shipRigidbody.AddForce(_v);
+			shipRigidbody.AddForce(_v*force);
 		}
 	}
 }

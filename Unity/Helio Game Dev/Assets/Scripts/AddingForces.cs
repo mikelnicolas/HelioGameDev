5a593e57a27ba5af1b437906c776aa5083745f73
Anthony Romrell <anthony@artisandream.com>
2017-09-09 11:34:57 -0600

    Added Force and Gravity.

---
+++ Unity/Helio Game Dev/Assets/Scripts/AddingForces.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddingForces : MonoBehaviour {
+
+	Rigidbody shipRigidbody;
+	public float force = 10;
+	public Vector3 forceVector;
+	void Start () {
+		shipRigidbody = GetComponent<Rigidbody>();
+	}
+	void Update () {
+		AddShipForce(KeyCode.UpArrow, Vector3.up*force);
+		AddShipForce(KeyCode.DownArrow, Vector3.down*force);
+		AddShipForce(KeyCode.LeftArrow, Vector3.forward*force);
+		AddShipForce(KeyCode.RightArrow, Vector3.back*force);
+	}
+
+	void AddShipForce (KeyCode _i, Vector3 _v) {
+		if (Input.GetKeyDown(_i))
+		{
+			shipRigidbody.AddForce(_v);
+		}
+	}
+}
--- Unity/Helio Game Dev/Assets/Scripts/GetHealth.cs
+++ Unity/Helio Game Dev/Assets/Scripts/GetHealth.cs
@@ -4,13 +4,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GetHealth : MonoBehaviour {
-	void Start () {
-		PlayerHealth.SendHealth = DisplayHealth;	
-	}
+public class GetHealth : MonoBehaviour
+{
+    void Start()
+    {
+        PlayerHealth.SendHealth = DisplayHealth;
+        PlayerHealth.SendHealth = cool;
+
+    }
+
+    private void cool(float obj)
+    {
+        throw new NotImplementedException();
+    }
 
     private void DisplayHealth(float _health)
     {
-		GetComponent<Text>().text = _health.ToString();
+        GetComponent<Text>().text = _health.ToString();
+
     }
+
+    void Update()
+    {
+        
+    }
+
 }

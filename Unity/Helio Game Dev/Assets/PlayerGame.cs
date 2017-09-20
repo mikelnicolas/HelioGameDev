66cf8b361d728059def42243dc1d1649ca588d71
Anthony Romrell <anthony@artisandream.com>
2017-09-19 19:14:47 -0600

    UI Start

---
+++ Unity/Helio Game Dev/Assets/PlayerGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class PlayerGame : MonoBehaviour {
+
+	public void Run () {
+		print("load");
+		SceneManager.LoadScene("Level0", LoadSceneMode.Additive);
+		gameObject.SetActive(false);
+
+	}
+
+}

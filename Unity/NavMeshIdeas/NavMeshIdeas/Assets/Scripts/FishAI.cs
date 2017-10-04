3a0f1f86a71e2ca7a478105c899745d734cc7821
Craig <lpforever18@gmail.com>
2017-10-03 20:16:56 -0600

    Merge

---
+++ Unity/NavMeshIdeas/NavMeshIdeas/Assets/Scripts/FishAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class FishAI : AI {
+    public RandomDestination fishpointList;
+
+    void Awake()
+	{
+		RandomDestination.SendThis += SendPointListHandler;
+	}
+
+    private void SendPointListHandler(RandomDestination _pointList)
+    {
+        fishpointList = _pointList;
+		food = fishpointList.ChangeDestination (fishpointList.points);
+    }
+
+	void OnTriggerEnter(Collider _c)
+	{
+		gameObject.SetActive(false);
+	}
+	
+}

7a33dd81f684b31c843c8da1f8c4a2bd4bba13cf
Anthony Romrell <anthony@artisandream.com>
2017-10-03 20:35:39 -0600

    Shark working, Bug is changing Fish

--- Unity/NavMeshIdeas/NavMeshIdeas/Assets/Scripts/SharkAI.cs
+++ Unity/NavMeshIdeas/NavMeshIdeas/Assets/Scripts/SharkAI.cs
@@ -7,10 +7,16 @@ using UnityEngine.AI;
 public class SharkAI : AI {
 
 	public RandomDestination foodSource;
+    public List<Transform> fish;
 
     void Awake()
     {
         RandomDestination.SendThis += SendThisHandler;
+        Invoke("GetFood", 0.1f);
+    }
+
+    void GetFood () {
+        food = foodSource.ChangeDestination(fish);
     }
 
     private void SendThisHandler(RandomDestination _rd)
